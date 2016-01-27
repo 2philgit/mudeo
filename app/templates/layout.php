934004974b6efcce883961e69552ffd3f871f597
odaydev <etienne.cenille@laposte.net>
2016-01-27 11:17:10 +0100

    intégration de la maquette avec le framework W

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -1,23 +1,194 @@
+<?php //session_start(); ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-	<meta charset="UTF-8">
-	<title><?= $this->e($title) ?></title>
+  
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <title><?= $this->e($title) ?></title>
+  
+  <!--CSS/reset+perso-->
+  <link rel="stylesheet" href="<?= $this->assetUrl('css/reset.css') ?>">
+  <link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
+  
+  <!--Font-->
+  <!--Favicon-->
+  <link rel="icon" type="image/png" href="<?= $this->assetUrl('img_site/favicon.png') ?>" />
+  <!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="img_site/favicon.png" /><![endif]-->
 
-	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
+  <!--jQuery CDN-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <!--Recaptcha Google-->
+  <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+  <!--<script src='https://www.google.com/recaptcha/api.js'></script>-->
+  
 </head>
 <body>
-	<div class="container">
-		<header>
-			<h1>W :: <?= $this->e($title) ?></h1>
-		</header>
+<script type="text/javascript">
+     // Load the SDK asynchronously
+    (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+<script>
+  // This is called with the results from from FB.getLoginStatus().
+  function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into Facebook.';
+    }
+  }
 
-		<section>
-			<?= $this->section('main_content') ?>
-		</section>
+  // This function is called when someone finishes with the Login
+  // Button.  See the onlogin handler attached to it in the sample
+  // code below.
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
 
-		<footer>
-		</footer>
-	</div>
+   window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1047080725312931',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+  };
+
+  // Now that we've initialized the JavaScript SDK, we call 
+  // FB.getLoginStatus().  This function gets the state of the
+  // person visiting this page and can return one of three states to
+  // the callback you provide.  They can be:
+  //
+  // 1. Logged into your app ('connected')
+  // 2. Logged into Facebook, but not your app ('not_authorized')
+  // 3. Not logged into Facebook and can't tell if they are logged into
+  //    your app or not.
+  //
+  // These three cases are handled in the callback function.
+
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+
+  // Here we run a very simple test of the Graph API after login is
+  // successful.  See statusChangeCallback() for when this call is made.
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      console.log(response);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+  }
+</script>
+
+
+
+  <div class="container">
+    <header>
+      <!-- <h1><?php //$this->e($title) ?></h1> -->
+      
+      
+
+    <nav class="wrap">
+
+      <!--Pour smartphone et tablette-->
+      <h1 id="logoMin"><a href="<?=$this->url('home') ?>" title="mudéo, renvoi à l'accueil"><img src="<?= $this->assetUrl('img_site/logo_min.png') ?>" alt="mudéo"></a></h1>
+      <!--Burger menu-->
+      <span id="burgerMenu"></span>
+      <!--Pour desktop-->
+      <h1 id="logo"><a href="<?=$this->url('home') ?>" title="mudéo, renvoi à l'accueil"><img src="<?= $this->assetUrl('img_site/logo.png') ?>" alt="mudéo"></a></h1>
+
+      <ul id="mainMenu" class="hide">
+        <li><a href="home.html">Regarder</a></li>
+        <li><a href="upload.html">Mettre en ligne</a></li>
+        <!--Apparait seulement si non-connecté-->
+        <li><a href="index.html#logger">Connection</a></li>
+        <!--Apparait seulement si connecté sur smartphone et tablette-->
+        <li><a class="desktopHide" href="profil.html">Profil</a></li>
+        <!--Seulement pour smartphone et tablette-->
+        <li><a class="desktopHide" href="helpCenter.html">Aide</a></li>
+        <!--Compte upgrade-->
+        <li><a href="helpCenter.html">Abonnement</a></li>
+      </ul>
+
+      <form id="searchField" method="GET">
+        <input id="search" type="text" name="searchInput" placeholder="Rechercher dans mudeo"></input>
+        <span id="searchIcon"><a href="" title="Rechercher"><img src="<?= $this->assetUrl('img_site/search.png') ?>" alt="Recherche icône"></a></span>
+      </form>
+      <a id="userProfil" href="profil.html" title="accès au profil"><img src="<?= $this->assetUrl('img_site/user.png') ?>" alt="profil"></a>
+
+    </nav>
+
+    <!-- <section> -->
+      <?= $this->section('main_content') ?>
+    <!-- </section> -->
+
+    <footer>
+
+          <!--Version Desktop uniquement-->
+        <section id="topPart">
+          <div id="centeredMenu">
+            <nav id="mudeoMenu">
+              <h5>Mudeo</h5>
+              <ul>
+                <li><a href="" title="A propos de mudeo">A propos de mudeo</a></li>
+                <li><a href="" title="Règles du site">Règles</a></li>
+                <li><a href="" title="Conditions d'utilisation">Conditions d'utilisation</a></li>
+                <li><a href="" title="Confidentialité">Confidentialité</a></li>
+              </ul>
+            </nav>
+            <nav id="aideMenu">
+              <h5>Aide</h5>
+              <ul>
+                <li><a href="" title="Centre d'aide">Centre d'aide</a></li>
+                <li><a href="" title="FAQ">FAQ</a></li>
+                <li><a href="" title="Droit d'auteur">Droit d'auteur</a></li>
+              </ul>
+            </nav>
+            <nav id="PlusMenu">
+              <h5>Plus</h5>
+              <ul>
+                <li><a href="" title="Mettre en ligne">Mettre en ligne</a></li>
+                <li><a href="" title="Plan du site">Plan du site</a></li>
+              </ul>
+            </nav>
+          </div>
+        </section>
+
+        <section id="bottomPart">
+          <span><p>&copy; 2016</p></span>
+          <a href="#mainMenu" title="Retour en haut"><img src="<?= $this->assetUrl('img_site/backtotop.png') ?>" alt="Retour en haut"></a>
+        </section>
+
+    </footer>
+      <script type="text/javascript" src="<?= $this->assetUrl('js/jquery.min.js') ?>"></script>
+      <script type="text/javascript" src="<?= $this->assetUrl('js/main.js') ?>"></script>
+
+  </div>
 </body>
 </html>
