38684223d491ad599bded8dfd34222c69f04a61a
2philgit <philippe.agm@gmail.com>
2016-01-28 07:27:35 +0100

    updade v0.3.2

--- app/Controller/SearchController.php
+++ app/Controller/SearchController.php
@@ -16,21 +16,12 @@ class SearchController extends Controller
 		// de la function resltSearch
 		$fileManager = new \Manager\FileManager;
 		$results = $fileManager->search();
-<<<<<<< HEAD
-
-		$this->showJson($results);
-		//$resultString = $fileManager->search();
-		//$this->show('search/result_search', [
-			//'results'=>$results,
-			//'resultString'=>$resultString,
-		//]);
-=======
 		$resultString = $fileManager->search();
 		$this->show('search/home_search', [
 			'results'=>$results,
 			'resultString'=>$resultString,
 		]);
->>>>>>> 5fd8052dc7fcfd563c061254294339eb821ef68d
+
 	}
 
 	/**
