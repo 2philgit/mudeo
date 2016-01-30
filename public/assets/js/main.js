4333dbebd4ed403dc3d9de33ee2e60f18706d9f6
jeremN <jeremne@gmail.com>
2016-01-30 21:32:30 +0100

    profil_modify.html
    Page upload -> ajout de div pour les erreurs (le h6 reste, mais dans la
    div/ vous pouvez mettre les id que vous voulez sur le h6).
    Ajout de la page profil_modify.
    Ajout fonction scroll
    Modif sur les bouton et lien

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,42 +1,51 @@
-//BurgerMenu animation
-$("#burger_Menu").on("click", function(e){
-	e.preventDefault();
-	$("#main_Menu").stop(true).slideToggle(250);
-	$("#burger_Menu").toggleClass("active");
+$(document).ready(function (){
+	//scroll
+    $("#back_To").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#top_Anchor").offset().top
+        }, 1000);
+    });
+
+	//BurgerMenu animation
+	$("#burger_Menu").on("click", function(e){
+		e.preventDefault();
+		$("#main_Menu").stop(true).slideToggle(250);
+		$("#burger_Menu").toggleClass("active");
+	});
+	//fin BurgerMenu animation
+
+
+	/*Inscription/Connection switch*/
+	//switch1
+	$("#join_Link").on("click", function(e){
+		e.preventDefault();
+		$("#repeat_Password").toggleClass("hide");
+		$(".g-recaptcha").toggleClass("hide");
+		$("#logger p").toggleClass("hide");
+		$("#switch_2").removeClass("hide");
+		$("#switch_1").addClass("hide");
+	});
+	//switch2
+	$("#connect_Link").on("click", function(e){
+		e.preventDefault();
+		$("#repeat_Password").toggleClass("hide");
+		$(".g-recaptcha").toggleClass("hide");
+		$("#logger p").toggleClass("hide");
+		$("#switch_1").removeClass("hide");
+		$("#switch_2").addClass("hide");
+	});
+	/*fin inscription/connection switch*/
+
+
+	/*MDP oublié*/
+	$("#forget_Mdp").on("click", function(e){
+		e.preventDefault();
+		$("#recover_Mdp").toggleClass("hide");
+		$("#recover_Button").toggleClass("hide");
+		$(".g-recaptcha").toggleClass("marge");//hack de merde
+	});
+	/*fin MDP oublié*/
+
 });
-//fin BurgerMenu animation
 
 
-/*Inscription/Connection switch*/
-//switch1
-$("#join_Link").on("click", function(e){
-	e.preventDefault();
-	$("#repeat_Password").toggleClass("hide");
-	$(".g-recaptcha").toggleClass("hide");
-	$("#logger p").toggleClass("hide");
-	$("#switch_2").removeClass("hide");
-	$("#switch_1").addClass("hide");
-});
-//switch2
-$("#connect_Link").on("click", function(e){
-	e.preventDefault();
-	$("#repeat_Password").toggleClass("hide");
-	$(".g-recaptcha").toggleClass("hide");
-	$("#logger p").toggleClass("hide");
-	$("#switch_1").removeClass("hide");
-	$("#switch_2").addClass("hide");
-});
-/*fin inscription/connection switch*/
-
-
-/*MDP oublié*/
-$("#forget_Mdp").on("click", function(e){
-	e.preventDefault();
-	$("#recover_Mdp").toggleClass("hide");
-	$("#recover_Button").toggleClass("hide");
-	$(".g-recaptcha").toggleClass("marge");//hack de merde
-});
-/*fin MDP oublié*/
-
-
-//scroll
